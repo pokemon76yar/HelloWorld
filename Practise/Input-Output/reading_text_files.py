4eefaf2c7589d7e01dc623c09beafae2aa273864
pokemon76 <sma40t@yandex.ru>
2023-01-16 07:03:01 +0300

    modules

--- Practise/Input-Output/reading_text_files.py
+++ Practise/Input-Output/reading_text_files.py
@@ -8,37 +8,37 @@
 # print(1, 2, 3, sep=',', end=' ')
 # print(1, 2, 3, sep=';', end='')
 
-# lorem_ipsum_text = open('/Users/yuriyallakhverdov/Desktop/sample.txt', 'r')
-# for line in lorem_ipsum_text:
-#     print(line,end='')
-# lorem_ipsum_text.close()
+lorem_ipsum_text = open('001 sample.txt', 'r')
+for line in lorem_ipsum_text:
+    print(line,end='')
+lorem_ipsum_text.close()
 
-# lorem_ipsum_text = open('/Users/yuriyallakhverdov/Desktop/sample.txt', 'r')
+# lorem_ipsum_text = open('001 sample.txt', 'r')
 # for line in lorem_ipsum_text:
 #     if 'mary' in line.lower():
 #         print(line,end='')
 # lorem_ipsum_text.close()
 
-# with open('/Users/yuriyallakhverdov/Desktop/sample.txt', 'r') as lorem_ipsum_text:
+# with open('001 sample.txt', 'r') as lorem_ipsum_text:
 #     for line in lorem_ipsum_text:
 #         if 'mary' in line.lower():
 #             print(line,end='')
 
-# with open('/Users/yuriyallakhverdov/Desktop/sample.txt', 'r') as lorem_ipsum_text:
+# with open('001 sample.txt', 'r') as lorem_ipsum_text:
 #     line = lorem_ipsum_text.readline()
 #     while line:
 #         print(line, end='')
 #         line = lorem_ipsum_text.readline()
 
-# with open('/Users/yuriyallakhverdov/Desktop/sample.txt', 'r') as lorem_ipsum_text:
+# with open('001 sample.txt', 'r') as lorem_ipsum_text:
 #     lines = lorem_ipsum_text.readlines()
 # print(lines)
 #
 # for line in lines[::-1]:
 #     print(line)
 
-with open('/Users/yuriyallakhverdov/Desktop/sample.txt', 'r') as lorem_ipsum_text:
-    text = lorem_ipsum_text.read()
-print(text)
+# with open('001 sample.txt', 'r') as lorem_ipsum_text:
+#     text = lorem_ipsum_text.read()
+# print(text)
 
 
--- Practise/Modules/external_modules.py
+++ Practise/Modules/external_modules.py
@@ -2,3 +2,7 @@ import termcolor
 
 print(termcolor.colored('Hello termcolor', 'green', 'on_yellow'))
 # help(termcolor)
+
+from termcolor import colored, cprint
+
+cprint("TESTING TERMCOLOR", "red")
